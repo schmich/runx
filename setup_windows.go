be9af84576d2da38b7e3cfcc4cb5b01de54ff24d
Chris Schmich <schmch@gmail.com>
2016-10-06 23:06:40 -0500

    Add build script for Windows. Split runtime setup into conditionally compiled files.

---
+++ setup_windows.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "strings"
+  "path"
+  "os"
+)
+
+func setupRuntime(dir string) string {
+  root := path.Join(dir, "runtime", "lib", "ruby")
+  ruby := path.Join(root, "bin.real", "ruby.exe")
+
+  version := "2.1.0"
+  arch := "i386-mingw32"
+
+  rubyLib := strings.Join([]string{
+    path.Join(root, "lib", "ruby", "site_ruby", version),
+    path.Join(root, "lib", "ruby", "site_ruby", version, arch),
+    path.Join(root, "lib", "ruby", "site_ruby"),
+    path.Join(root, "lib", "ruby", "vendor_ruby", version),
+    path.Join(root, "lib", "ruby", "vendor_ruby", version, arch),
+    path.Join(root, "lib", "ruby", "vendor_ruby"),
+    path.Join(root, "lib", "ruby", version),
+    path.Join(root, "lib", "ruby", version, arch),
+  }, ";")
+  os.Setenv("RUBYLIB", rubyLib)
+
+  return ruby
+}
+
