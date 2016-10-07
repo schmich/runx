d48d4392bd192c1507383efb062cfee41dd8fd04
Chris Schmich <schmch@gmail.com>
2016-10-07 05:58:43 -0500

    Update OSX Ruby package.

--- build-osx.sh
+++ build-osx.sh
@@ -2,10 +2,12 @@
 
 set -x
 
+package=traveling-ruby-20150210-2.1.5-osx.tar.gz
+
 if [ ! -d runtime ]; then
   mkdir -p runtime/lib/ruby
   mkdir -p runtime/lib/app
-  curl -L --fail https://d6r77u77i8pq3.cloudfront.net/releases/traveling-ruby-20141215-2.1.5-osx.tar.gz | tar -zxv -C runtime/lib/ruby
+  curl -L --fail "https://d6r77u77i8pq3.cloudfront.net/releases/$package" | tar -zxv -C runtime/lib/ruby
 fi
 
 cp runx.rb runtime/lib/app/runx.rb
