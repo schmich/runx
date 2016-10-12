01398e14fea54e18c72b9b0ce31b9952fe5a1776
Chris Schmich <schmch@gmail.com>
2016-10-12 09:24:01 -0500

    Convert Linux Docker build script to Ruby. This allows us to build the Linux image across platforms.

---
+++ build-linux-docker.rb
@@ -0,0 +1,17 @@
+#!env ruby
+
+match = File.read('main.go').match(/Version.*(\d+\.\d+\.\d+)/)
+version = match[1]
+if !version
+  puts 'Version not found.'
+  exit 1
+end
+
+target = "runx-linux-x64-#{version}"
+system('docker build -t runx .')
+system('docker run -it runx')
+id = `docker ps -l -q`.strip
+system("docker cp #{id}:/src/runx ./#{target}")
+system("docker rm #{id}")
+system("docker run -it --rm -v #{Dir.pwd}/#{target}:/bin/runx golang:1-wheezy bash")
+system("docker rmi runx:latest")
