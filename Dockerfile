35a74f6f102b6399c90cf80b6db05c7cb7ced5e6
Chris Schmich <schmch@gmail.com>
2016-10-12 09:04:02 -0500

    Make Linux building self-contained.

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ MAINTAINER Chris Schmich <schmch@gmail.com>
 RUN go get github.com/kardianos/osext \
  && go get github.com/mitchellh/go-homedir \
  && go get github.com/jteeuwen/go-bindata/...
+COPY . /src
 WORKDIR /src
 CMD ["/bin/bash", "-c", "/src/build-linux.sh"]
--- build-linux-docker.sh
+++ build-linux-docker.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+version=`grep Version main.go | egrep -o '\d+\.\d+\.\d+'`
+target="runx-linux-x64-$version"
 docker build -t runx .
-docker run -it --rm -v `pwd`:/src runx
-docker run -it --rm -v `pwd`/runx:/bin/runx golang:1-wheezy bash
+docker run -it runx
+id=`docker ps -l -q`
+docker cp $id:/src/runx "./$target"
+docker rm $id
+docker run -it --rm -v "`pwd`/$target":/bin/runx golang:1-wheezy bash
+docker rmi runx:latest
