9c8edcc1c1e42b89f6318d23d630bd333a989fd9
Chris Schmich <schmch@gmail.com>
2020-07-15 22:47:05 -0700

    Add Docker shell task.

--- Runfile.rb
+++ Runfile.rb
@@ -4,13 +4,18 @@ end
 
 def run(command)
   if is_vm?
-    system command
+    system(command) || fail
   else
     system "vagrant ssh -c \"#{command}\""
   end
 end
 
-task 'Build runx binary: linux darwin windows'
+task 'Build runx: linux darwin windows'
 def build(*platforms)
   run "cd /src && sudo docker build -t runx . && sudo docker run --rm -v $(pwd):/src runx ruby build.rb #{platforms.join(' ')}"
+end
+
+task 'Start Docker shell'
+def shell
+  run 'cd /src sudo docker build -t runx . && sudo docker run --rm -it -v $(pwd):/src runx /bin/bash'
 end
