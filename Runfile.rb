877999e49eb5453fdeb09759284bb27aee50d362
Chris Schmich <schmch@gmail.com>
2020-07-19 20:43:26 -0700

    Update Runfile to run commands on host or guest VM.

--- Runfile.rb
+++ Runfile.rb
@@ -1,21 +1,61 @@
+require 'json'
+
 def is_vm?
   `whoami`.strip == 'vagrant'
 end
 
-def run(command)
-  if is_vm?
-    system(command) || fail
-  else
-    system "vagrant ssh -c \"#{command}\""
-  end
+def image_exists?
+  `sudo docker images runx --format "{{json .}}"`.lines.any?
+end
+
+def ensure_image
+  create_image if !image_exists?
+end
+
+def create_image
+  system('cd /src && sudo docker build -t runx .') || fail
+end
+
+def delete_image
+  system('sudo docker rmi --force runx') || fail
 end
 
 task 'Build runx: linux darwin windows'
-def build(*platforms)
-  run "cd /src && sudo docker build -t runx . && sudo docker run --rm -v $(pwd):/src runx ruby build.rb #{platforms.join(' ')}"
+def build(*platform)
+  if is_vm?
+    ensure_image
+    system("sudo docker run --rm -v /src:/src runx ruby build.rb #{platform.join(' ')}") || fail
+  else
+    system("vagrant ssh -c \"cd /src && runx build #{platform.join(' ')}\"")
+  end
 end
 
-task 'Start Docker shell'
-def shell
-  run 'cd /src sudo docker build -t runx . && sudo docker run --rm -it -v $(pwd):/src runx /bin/bash'
+task 'Create Docker image'
+def docker_create
+  if is_vm?
+    delete_image
+    create_image
+  else
+    system("vagrant ssh -c \"cd /src && runx docker build\"")
+  end
+end
+
+task 'Start interactive Docker shell'
+def docker_shell
+  if is_vm?
+    ensure_image
+    system('sudo docker run --rm -it -v /src:/src runx /bin/bash')
+  else
+    system("vagrant ssh -c \"cd /src && runx docker shell\"")
+  end
+end
+
+task 'Run Docker command'
+def docker_run(cmd, *args)
+  if is_vm?
+    ensure_image
+    system("sudo docker run --rm -v /src:/src runx #{cmd} #{args.join(' ')}")
+  else
+    system("vagrant ssh -c \"cd /src && runx docker run #{cmd} #{args.join(' ')}\"")
+  end
 end
